2b76c8d0631d89cb967813e564386f020c7c9d46
Tanzhor_RIK <tanzhor4@gmail.com>
2023-10-02 21:55:16 +0700

    Add models

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django_celery_beat.models import PeriodicTask
 
-# Create your models here.
+from users.models import User, NULLABLE
+
+
+class Habit(models.Model):
+    """Модель привычки"""
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Владелец", **NULLABLE)
+    place = models.CharField(max_length=150, verbose_name="Место выполнения")
+    time = models.TimeField(default=timezone.now, verbose_name="Время выполнения")
+    action = models.CharField(max_length=150, null=False, blank=False, verbose_name="Действие")
+    is_pleasant = models.BooleanField(default=False, verbose_name="Флаг привычки")
+    link_pleasant = models.ForeignKey("self", on_delete=models.SET_NULL, verbose_name='Связанная привычка', **NULLABLE)
+    award = models.CharField(max_length=150, verbose_name="Вознаграждение", **NULLABLE)
+    frequency = models.PositiveIntegerField(default=1, verbose_name='Периодичность')
+    duration = models.PositiveIntegerField(default=120, verbose_name='Время на выполнение')
+    is_public = models.BooleanField(default=False, verbose_name='Флаг публикации')
+    task = models.ForeignKey(PeriodicTask, on_delete=models.SET_NULL, verbose_name='Ссылка на задачу',
+                             **NULLABLE)
+
+    def __str__(self):
+        loop_self = self
+        message = f"Я буду {self.action} в {self.time} по адресу {self.place}\nВремя на выполнение: {self.duration}\n"
+        while True:
+            if loop_self.award:
+                return message + f"Вознаграждение: {loop_self.award}"
+            elif loop_self.link_pleasant is None:
+                return message
+            else:
+                loop_self = loop_self.link_pleasant
+                continue
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = 'Привычки'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+    username = models.CharField(unique=True, max_length=50, verbose_name="Имя пользователя")
+    tg_user_id = models.IntegerField(verbose_name="id", **NULLABLE)
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = 'Пользователи'
